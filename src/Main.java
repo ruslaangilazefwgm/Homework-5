80120e3dbcb947a04062dd779e6a362343776331
ruslaan.gilazefwgm <ruslaan.gilazefwgm@gmail.com>
2022-09-12 15:41:15 +0300

    Переменные. Часть 2

--- src/Main.java
+++ src/Main.java
@@ -31,8 +31,58 @@ public class Main {
         //Задача 3
         System.out.println("Задача 3");
 
+        int year = 2021;
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println("Год високосный");
+         }
+        else if (year % 4 != 0 || year % 100 == 0) {
+            System.out.println("Год не високосный");
+        }
+
+        //Задача 4
+        System.out.println("Задача 4");
+
+        int deliveryDistance = 95;
+        if (deliveryDistance < 20) {
+            System.out.println("Потребуется 1 день");
+        }
+        else if (deliveryDistance >= 20 && deliveryDistance < 60) {
+            System.out.println("Потребуется 2 дня");
+        }
+        else if (deliveryDistance >= 60 && deliveryDistance < 100) {
+            System.out.println("Потребуется 3 дня");
+        }
+
+        //Задача 5
+        System.out.println("Задача 5");
+
+        int monthNumber = 12;
+        switch (monthNumber) {
+            case 1:
+            case 2:
+            case 12:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Такого месяца не существует");
+        }
 
 
 
+        }
     }
-}
